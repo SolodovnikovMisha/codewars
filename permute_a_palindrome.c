88c4f27981b469c4f47171daed831936dee66469
MishaSolodovnikov <solodovnikov2110@gmail.com>
2023-07-22 12:19:27 +0300

    permute_a_palindrome

---
+++ permute_a_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+
+bool permute_a_palindrome(const char *str_in) 
+{
+  int count = 0;
+  static int symbol_frequency[26];
+  for(int i = 0; i < 26; i++)
+    symbol_frequency[i] = 0;
+  for(int i = 0; *(str_in + i) != '\0'; i++)
+    symbol_frequency[*(str_in + i) - 'a']++;
+  for(int i = 0; i < 26; i++)
+    if(symbol_frequency[i] % 2 == 1)
+      count++;
+  return count<2?1:0;
+}
