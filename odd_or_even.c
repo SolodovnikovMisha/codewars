83a0586466cfa551d0f9d64182549eef25305784
MishaSolodovnikov <solodovnikov2110@gmail.com>
2023-07-22 12:23:55 +0300

    odd_or_even

---
+++ odd_or_even.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+const char *odd_or_even(const int *v, size_t sz)
+{
+  int sum = 0;
+  for(int i = 0; i < sz; i++)
+  {
+    sum += v[i];
+  }
+  return (abs(sum) % 2)? "odd": "even"; 
+}
